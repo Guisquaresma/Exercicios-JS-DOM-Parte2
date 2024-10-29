a7ccd39e5a7cd370215e0b5843dc6a2210a26d80
Guilherme Santos Quaresma <gsqguilhermesantos@gmail.com>
2024-10-29 13:56:07 -0300

    Atualização

--- script.js
+++ script.js
@@ -13,8 +13,35 @@ let alteraTexto = () => {
 
 // 3ª
 let novoItem = () => {
-    const adicionarItem = document.createElement('li');
-    adicionarItem.innerHTML += "Novo Item";
-    minhaLista.appendChild(adicionarItem);
+    minhaLista.innerHTML += "<li>Novo Item</li>";
 }
 
+// 4ª
+function adicionarItem () {
+    let criar = document.createElement('li');
+    criar.innerHTML = "Item Criado Dinamicamente";
+    minhaLista.appendChild(criar);
+}
+
+// 5ª
+let senha = document.querySelector('input');
+
+function verSenha() {
+    if (senha.getAttribute('type') === 'password') {
+        senha.setAttribute('type', 'text');
+    } else {
+        senha.setAttribute('type', 'password');
+    }
+}
+
+// 6ª
+let alterarCor = () => {
+    minhaLista.children[2].style.color = "blue";
+    minhaLista.children[3].style.fontWeight = "bold";
+}
+
+// 7ª
+let antes = () => {
+    minhaLista.before("<< Antes da lista >>");
+    minhaLista.after("<< Depois da Lista >>");
+}
